243f14ba41a649b8795c1ae2a734781a6930d607
Tsutaya Koki <haku.355350@gmail.com>
2025-11-06 12:25:05 +0900

    change

--- test.bash
+++ test.bash
@@ -13,11 +13,11 @@ out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
 ###STRANGE INPUT ###
-out=$(echo あ | ./plus)
+out=$(echo 5 | ./plus)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
+out=$(echo 5 | ./plus)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
