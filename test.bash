fc95bfd8eb58c2b887a09d1f4bfccad88c390c0d
Tsutaya Koki <haku.355350@gmail.com>
2025-11-30 13:49:31 +0900

    fix test.bash

--- test.bash
+++ test.bash
@@ -10,32 +10,34 @@ ng () {
 res=0
 
 ### NORMAL INPUT ###
-out=$(echo "1" | ./element_lookup.py)
+out=$(echo "1" | ./element_lookup.py 2>/dev/null)
 expected="H"
 [ "$out" = "$expected" ] || ng "$LINENO"
 
-out=$(echo "26" | ./element_lookup.py)
+out=$(echo "26" | ./element_lookup.py 2>/dev/null)
 expected="Fe"
 [ "$out" = "$expected" ] || ng "$LINENO"
 
 ### MULTIPLE INPUT ###
-out=$(printf "1\n2\n26\n" | ./element_lookup.py)
+out=$(printf "1\n2\n26\n" | ./element_lookup.py 2>/dev/null)
 expected=$'H\nHe\nFe'
 [ "$out" = "$expected" ] || ng "$LINENO"
 
-### INVALID INPUT (非数字) ###
-out=$(echo "abc" | ./element_lookup.py)
+### INVALID INPUT（非数字）###
+err=$(echo "abc" | ./element_lookup.py 1>/dev/null 2>&1)
 expected="invalid input"
-[ "$out" = "$expected" ] || ng "$LINENO"
+[ "$err" = "$expected" ] || ng "$LINENO"
 
-### INVALID NUMBER (範囲外) ###
-out=$(echo "119" | ./element_lookup.py)
+### INVALID NUMBER（範囲外）###
+err=$(echo "119" | ./element_lookup.py 1>/dev/null 2>&1)
 expected="invalid number"
-[ "$out" = "$expected" ] || ng "$LINENO"
+[ "$err" = "$expected" ] || ng "$LINENO"
 
 ### EMPTY INPUT ###
-out=$(cat /dev/null | ./element_lookup.py)
+out=$(cat /dev/null | ./element_lookup.py 2>&1)
 ret=$?
+
+# Usage は stderr に出力
 [ "$ret" = 0 ] || ng "$LINENO"
 [[ "$out" == *Usage* ]] || ng "$LINENO"
 
