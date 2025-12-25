36e29f158f45699848735dd179882651c1ac475e
Tsutaya Koki <haku.355350@gmail.com>
2025-12-25 12:23:58 +0900

    Update

--- test.bash
+++ test.bash
@@ -10,31 +10,31 @@ ng () {
 res=0
 
 ### NORMAL INPUT ###
-out=$(echo "1" | ./element_lookup.py 2>/dev/null)
+out=$(echo "1" | ./element_lookup 2>/dev/null)
 expected="H"
 [ "$out" = "$expected" ] || ng "$LINENO"
 
-out=$(echo "26" | ./element_lookup.py 2>/dev/null)
+out=$(echo "26" | ./element_lookup 2>/dev/null)
 expected="Fe"
 [ "$out" = "$expected" ] || ng "$LINENO"
 
 ### MULTIPLE INPUT ###
-out=$(printf "1\n2\n26\n" | ./element_lookup.py 2>/dev/null)
+out=$(printf "1\n2\n26\n" | ./element_lookup 2>/dev/null)
 expected=$'H\nHe\nFe'
 [ "$out" = "$expected" ] || ng "$LINENO"
 
 ### INVALID INPUT (非数字) ###
-err=$(echo "abc" | ./element_lookup.py 2>&1 1>/dev/null)
+err=$(echo "abc" | ./element_lookup 2>&1 1>/dev/null)
 expected="invalid input"
 [ "$err" = "$expected" ] || ng "$LINENO"
 
 ### INVALID NUMBER (範囲外) ###
-err=$(echo "119" | ./element_lookup.py 2>&1 1>/dev/null)
+err=$(echo "119" | ./element_lookup 2>&1 1>/dev/null)
 expected="invalid number"
 [ "$err" = "$expected" ] || ng "$LINENO"
 
 ### EMPTY INPUT ###
-out=$(cat /dev/null | ./element_lookup.py 2>&1)
+out=$(cat /dev/null | ./element_lookup 2>&1)
 ret=$?
 [ "$ret" = 0 ] || ng "$LINENO"
 [[ "$out" == *Usage* ]] || ng "$LINENO"
